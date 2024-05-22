4914cc8e0c227712d5ece2e29614da11c3058080
punyaphatsura <punyaphat.sura@gmail.com>
2024-05-22 17:48:00 +0700

    [3-1] Com sys arch

---
+++ 3-1/Com-Sys-Arch/HW/CacheSim.h
@@ -0,0 +1,34 @@
+/**
+ * CacheSim.h
+ * This program is designed for class exercise only.
+ * It is provided as is. There is no warranty or support of any kind.
+ *
+ * Krerk Piromsopa, Ph.D.
+ * Department of Computer Engineering
+ * Chulalongkorn University
+ * Bangkok, Thailand.
+ **/
+
+#include <math.h>
+
+#ifndef CACHESIM_H_INCLUDED
+#define CACHESIM_H_INCLUDED
+
+/* Please adjust parameters here */
+#define CACHE_SIZE 1024 * 1
+#define BLOCK_SIZE 4
+#define INDEX_SIZE (CACHE_SIZE / (BLOCK_SIZE / 2))
+// #define TAGLEN 22
+#define INDEXLEN (int)(log2(INDEX_SIZE))
+#define OFFSETLEN (int)(log2(BLOCK_SIZE))
+typedef unsigned char Byte;
+typedef unsigned char bool;
+typedef struct Cache
+{
+    bool valid;
+    bool dirty;
+    unsigned long tag;
+    Byte data[BLOCK_SIZE];
+};
+
+#endif // CACHESIM_H_INCLUDED
